64780b729e9ea91e6d033e95b935766a1f94c8a1
James Hamm <jameshamm1995@gmail.com>
2015-04-28 16:18:21 +0100

    added files
    Added header.php

---
+++ header.php
@@ -0,0 +1,60 @@
+<?php
+echo   '<header>
+		<!-- Main header and navigation bar with drop down menus -->
+			<nav>
+				<ul class = "menu">
+					<li>
+						<a href = "homepage.php"><img src = "../Images/zombie.png"/></a>
+						<a href = "homepage.php">Music Zombies</a>
+					</li>
+					<li>
+						<a href="#">Music</a><!-- Drop down menu title-->
+						<ul>
+							<!-- Drop down menu links -->
+							<li>
+								<a href = "#">CD\'s</a><!-- # is placeholder-->
+							</li>
+							<li>
+								<a href = "#">Digital</a><!-- # is placeholder-->
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Merchandise </a><!-- Drop down menu title-->
+						<ul>
+							<!-- Drop down menu links -->
+							<li>
+								<a href = "#">T-Shirts</a><!-- # is placeholder-->
+							</li>
+							<li>
+								<a href = "#">Cups</a><!-- # is placeholder-->
+							</li>
+							<li>
+								<a href = "#">Posters</a><!-- # is placeholder-->
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Sign in</a><!-- Drop down menu title-->
+						<ul>
+						<!-- Drop down menu links -->
+							<li>
+								<form action="demo_form.asp">
+									Email:<input type="text" name="Log in">
+									Password:<input type="text" name="password">
+									<input type="submit" value="Enter">
+								</form>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<form action="Results_Page.php" method="post">
+							<input type="text" name="field"/> <!--Hamza & Jeanette 5eva-->
+							<input type="submit" name="submit" value="submit"/>
+						</form>
+					</li>
+				</ul>
+				<a href = "#"><img src = "../Images/cart.png" id = "cart"/></a>
+			</nav>
+		</header>';
+?>
