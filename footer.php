01bfadc81c905c2fdbc794251ee273bf3ee7cae3
James Hamm <jameshamm1995@gmail.com>
2015-04-28 16:28:26 +0100

    added footer.php
    Added footer.php and updated header.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+echo   '<footer>
+			<!-- Footer needs to have alot more -->
+			<p>&#169; Team A3</p>
+			<br>
+			<p> About Us </p>
+			<p> Contact info </p>
+			<p> T & C </p>
+			<p> Help </p>
+		</footer>';
+?>
