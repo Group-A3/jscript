9c13246a41b6c2bc1653ed1fb863a5f54ad1bf9d
TheToby1 <toby8urns@gmail.com>
2015-05-03 17:36:52 +0100

    stuff and shizzle

--- imageswitch.js
+++ imageswitch.js
@@ -8,6 +8,14 @@ function cycleItems() {
   item.css('display','inline-block');
 }
 
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
 $('.next').click(function() {
   currentIndex += 1;
   if (currentIndex > itemAmt - 1) {
